3527635e061004745671e1b7bed0b462be00c934
Syntasu <abouma18@hotmail.com>
2019-01-16 14:14:32 +0100

    Patch up the gcc build file

--- gccbuild.bat
+++ gccbuild.bat
@@ -1,2 +1,2 @@
 cd utttbot
-g++ Common.h Settings.cpp Command.h CommandProcessor.cpp Field.h Macrofield.cpp FieldManager.cpp MoveProcessor.cpp main.cpp
+g++ Common.h Settings.cpp Command.h CommandProcessor.cpp Field.h Macrofield.cpp FieldManager.cpp Evaluator.cpp MoveProcessor.cpp main.cpp
